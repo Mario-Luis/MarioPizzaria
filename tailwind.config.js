476d282b3ff557f24ffc5714c2f3045cf4c4da7b
Mario Luis <yamamotomarioluis@gmail.com>
2024-10-17 21:15:37 +0900

    MarioPizzaria

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "home": "url('/assets/IMG_7808.JPG')"
+      }
+    },
+  },
+  plugins: [],
+}
+
